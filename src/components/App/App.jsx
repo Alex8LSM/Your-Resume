e7a2aba8b7e240b1f243cb9168cd849c7f56fef3
Oleksandr Khvaleba <alex8inbox@gmail.com>
2022-07-30 18:57:33 +0300

    spacerFix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,27 +8,27 @@ import Sidebar from '../Sidebar/Sidebar';
 import Contacts from '../Contacts/Contacts';
 import TechSkills from '../TechSkills/TechSkills';
 import SoftSkills from '../SoftSkills/SoftSkills';
-import user from '../../Data/user.json'
+import user from '../../Data/user.json';
 export const App = () => {
   return (
     // <!--Main_conteiner-->
     <Section className={styles.mainContainer}>
-        {/* <!--Rectangle_1--> */}
-        <Section className={styles.container}>
-            {/* <!--Main_content_section--> */}
-            <Section className={styles.mainContent}>
-                <AboutMe data={user.aboutMe}/>
-                <Projects projects={user.projects}/>
-                <WorkExperience companies={user.workExperience}/>
-                <Education universities={user.education}/>
-            </Section>
-            {/* <!--sidebar_section--> */}
-            <Sidebar photo={user.photo}>
-                <Contacts contacts={user.contacts}/>
-                <TechSkills skills={user.techSkills}/>
-                <SoftSkills skills={user.softSkills}/>
-            </Sidebar>
+      {/* <!--Rectangle_1--> */}
+      <Section className={styles.container}>
+        {/* <!--Main_content_section--> */}
+        <Section className={styles.mainContent}>
+          <AboutMe data={user.aboutMe} />
+          <Projects projects={user.projects} />
+          <WorkExperience companies={user.workExperience} />
+          <Education universities={user.education} />
         </Section>
-    </Section> 
+        {/* <!--sidebar_section--> */}
+        <Sidebar photo={user.photo}>
+          <Contacts contacts={user.contacts} />
+          <TechSkills skills={user.techSkills} />
+          <SoftSkills skills={user.softSkills} />
+        </Sidebar>
+      </Section>
+    </Section>
   );
 };
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,32 @@
 import styles from './Projects.module.css';
 
-const Projects = ({projects}) => {
-const progectList=projects.map((project,key)=>(
-    <li className={styles.projectsItem} key={key}>
-        <a href={project.link} className={styles.projectsLink}>{project.link}</a>
-        <span className={styles.projectsText}>...................................
-            <span className={styles.projectsBracket}>[</span> 
-             {project.tehnology} 
-            <span className={styles.projectsBracket}>]</span>
-        </span>
+const Projects = ({ projects }) => {
+  const progectList = projects.map((project, key) => (
+    <li className={styles.item} key={key}>
+      <div className={styles.itemContainer}>
+        <div className={styles.linkBlock}>
+          <a href={project.link} className={styles.link}>
+            {project.link}
+          </a>
+          <span className={styles.spacer}>
+            .................................................................................
+          </span>
+        </div>
+        <div className={styles.tehnologyBlock}>
+          <span className={styles.bracket}>[ </span>
+          <span className={styles.tehnology}>{project.tehnology}</span>
+          <span className={styles.bracket}> ]</span>
+        </div>
+      </div>
     </li>
-    ))
+  ));
   return (
     <section className={styles.container}>
       <h3 className={styles.mainTitle}>Projects</h3>
-  
-      <ol className={styles.projectsList}>
-      {progectList}
-      </ol>
 
-
+      <ol className={styles.projectsList}>{progectList}</ol>
     </section>
-  )
+  );
 };
 
 export default Projects;
